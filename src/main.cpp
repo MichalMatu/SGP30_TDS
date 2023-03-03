0b26dba88ffbd5cc642e9653962d39c076ab4e58
Michal Matuszewski <michal.matuszewski1984@gmail.com>
2023-03-03 10:25:20 +0100

    Starter

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 #include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
 
-void setup() {
-  // put your setup code here, to run once:
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup()
+{
+  lcd.init();      // Initialize the LCD
+  lcd.backlight(); // Turn on the backlight
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+
+  lcd.setCursor(0, 0); // Set the cursor to the first column and first row
+  lcd.print("Hello "); // Print the text "Temp: " on the LCD screen
 }
